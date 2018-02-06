49de9bd0418eedebdb0133e1ec388b06bdb9bf33
Johannes Bader <jobader@microsoft.com>
2018-02-05 23:20:25 -0800

    vanilla browser port

--- src/view-browser.js
+++ src/view-browser.js
@@ -1,36 +1,70 @@
 /*jshint esversion: 6 */
+  
+class OpenFileDialog {
+    constructor(inputElement) {
+        this.inputElement = inputElement;
+    }
 
-class BrowserHostService {
+    /**
+     * @param {(file: File) => void} handler 
+     */
+    onOpenFile(handler) {
+        this.inputElement.addEventListener("change", () => {
+            handler(this.inputElement.files && this.inputElement.files[0] || null);
+        });
+    }
+    
+    showOpenFileDialog() {
+        this.inputElement.click();
+    }
+}
+
+class ElectronHostService {
 
     constructor() {
     }
 
     initialize(callback) {
         this.callback = callback;
+        const openFileDialog = new OpenFileDialog(document.getElementById('open-file-dialog'));
+        
+        updateView('welcome');
+        
+        const onFile = file => {
+            if (file) {
+                updateView('spinner');
+                this.openBuffer(file);
+            }
+        };
+        openFileDialog.onOpenFile(onFile);
     
-        updateView('spinner');
+        var openFileButton = document.getElementById('open-file-button');
+        if (openFileButton) {
+            openFileButton.style.opacity = 1;
+            openFileButton.addEventListener('click', (e) => {
+                openFileButton.style.opacity = 0;
+                openFileDialog.showOpenFileDialog();
+            });
+        }
 
-        var request = new XMLHttpRequest();
-        request.responseType = 'arraybuffer';
-        request.onload = () => {
-            if (request.status == 200) {
-                this.callback(null, new Uint8Array(request.response), document.title);
-            }
-            else {
-                this.callback(request.status, null);
-            }
-        };
-        request.onerror = () => {
-            this.callback(request.status, null);
-        };
-        request.open('GET', '/data', true);
-        request.send();
+        document.addEventListener('dragover', (e) => {
+            e.preventDefault();
+        });
+        document.addEventListener('drop', (e) => {
+            e.preventDefault();
+        });
+        document.body.addEventListener('drop', (e) => { 
+            e.preventDefault();
+            var files = e.dataTransfer.files;
+            onFile(files && files[0] || null);
+            return false;
+        });
     }
-    
+
     showError(message) {
         alert(message);
     }
-    
+
     request(file, callback) {
         var request = new XMLHttpRequest();
         if (file.endsWith('.pb')) {
@@ -53,13 +87,31 @@ class BrowserHostService {
         request.onerror = () => {
             callback(request.status, null);
         };
-        request.open('GET', file, true);
+        request.open('GET', '.' + file, true);
         request.send();
     }
 
     openURL(url) {
         window.open(url, '_target');
     }
+
+    /**
+     * @param {File} file 
+     */
+    openBuffer(file) {          
+        var size = file.size;
+        const fileReader = new FileReader();
+        fileReader.onloadend = () => {
+            if (fileReader.error) {
+                this.callback(fileReader.error, null, null);
+            }
+            else {
+                var buffer = new Uint8Array(fileReader.result);
+                this.callback(null, buffer, file.name);
+            }
+        };
+        fileReader.readAsArrayBuffer(file);
+    }
 }
 
-var hostService = new BrowserHostService();
+var hostService = new ElectronHostService();
